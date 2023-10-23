b16973cceceff953745810fc29365c33127d8ccc
Leonardo Christ <christ.leonardo21@gmail.com>
2023-10-23 14:08:59 -0300

    duvidas

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.date').mask('00/00/0000');
+    $('.time').mask('00:00:00');
+    $('.date_time').mask('00/00/0000 00:00:00');
+    $('.cep').mask('00000-000');
+    $('#telefone').mask('0000-0000');
+    $('.phone_with_ddd').mask('(00) 0000-0000');
+})
