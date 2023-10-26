67d177e05b4f42c5869f7f1fa8ea45cedb330040
Leonardo Christ <christ.leonardo21@gmail.com>
2023-10-26 11:12:03 -0300

    commit final

--- assets/main.js
+++ assets/main.js
@@ -2,4 +2,17 @@
         $('#telefone').inputmask('(99) 99999-9999');
         $('#nome').attr("placeholder", "João Silva")
         $('#email').attr("placeholder", "joão.silva@exemplo.com")
+        $('form').validade({
+            rules: {
+                nome: {
+                    required: true
+                },
+                email: {
+                    required: true
+                },
+                telefone: {
+                    required: true
+                }
+            }
+        })
     })
